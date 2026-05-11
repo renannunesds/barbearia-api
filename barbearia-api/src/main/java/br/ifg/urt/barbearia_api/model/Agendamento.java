ed3d824b9e2b9fc3d684fa39c8e37178fc01e4e7
2024101202010016 <daniellrinconn@gmail.com>
2026-05-11 10:08:21 -0300

    teste

--- barbearia-api/src/main/java/br/ifg/urt/barbearia_api/model/Agendamento.java
+++ barbearia-api/src/main/java/br/ifg/urt/barbearia_api/model/Agendamento.java
@@ -1,4 +1,68 @@
 package br.ifg.urt.barbearia_api.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "agendamento")
 public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataHorario;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "barbeiro_id")
+    private Barbeiro barbeiro;
+
+    public Agendamento() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataHorario() {
+        return dataHorario;
+    }
+
+    public void setDataHorario(LocalDateTime dataHorario) {
+        this.dataHorario = dataHorario;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Barbeiro getBarbeiro() {
+        return barbeiro;
+    }
+
+    public void setBarbeiro(Barbeiro barbeiro) {
+        this.barbeiro = barbeiro;
+    }
 }
