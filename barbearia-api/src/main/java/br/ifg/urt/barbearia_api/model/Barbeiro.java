aefe403a8ac4d3a24262c0beaffa1734cd1a1222
Ryan Victor <ryanv9695@gmail.com>
2026-05-11 15:27:04 -0300

    feat: as classes Agendamento, Barbeiro

--- barbearia-api/src/main/java/br/ifg/urt/barbearia_api/model/Barbeiro.java
+++ barbearia-api/src/main/java/br/ifg/urt/barbearia_api/model/Barbeiro.java
@@ -1,4 +1,42 @@
 package br.ifg.urt.barbearia_api.model;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Barbeiro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idBarbeiro;
+
+    private String nome;
+
+    private String telefone;
+
+    // getters e setters
+
+    public Long getIdBarbeiro() {
+        return idBarbeiro;
+    }
+
+    public void setIdBarbeiro(Long idBarbeiro) {
+        this.idBarbeiro = idBarbeiro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
